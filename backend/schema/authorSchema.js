97b43f43e1080ae4b5a5ebedf0309e596e7beb29
nihardarji <ndarji@hawk.iit.edu>
2021-02-26 22:59:42 -0600

    added relation between author and book

--- backend/schema/authorSchema.js
+++ backend/schema/authorSchema.js
@@ -1,11 +1,19 @@
-import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLString } from "graphql"
+import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql'
+import { BookType } from './bookSchema.js'
+import { books } from './rootSchema.js'
 
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books.filter(book => book.authorId === parent.id)
+            }
+        }
     })
 })
 
--- backend/schema/bookSchema.js
+++ backend/schema/bookSchema.js
@@ -1,11 +1,19 @@
-import { GraphQLID, GraphQLObjectType, GraphQLString } from "graphql"
+import { GraphQLID, GraphQLObjectType, GraphQLString } from 'graphql'
+import { AuthorType } from './authorSchema.js'
+import { authors } from './rootSchema.js'
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return authors.find(author => author.id === parent.authorId)
+            }
+        }
     })
 })
 
