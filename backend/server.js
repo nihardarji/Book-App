56516bed2564eed16a4b504d5ca5a733258df231
nihardarji <ndarji@hawk.iit.edu>
2021-02-28 01:55:02 -0600

    resolved deploying issue

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,13 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
-if(process.env.NODE_ENV === 'production'){
+const __dirname = path.resolve()
+
+// if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-}
+// }
 
 const PORT = process.env.PORT || 5000
 
