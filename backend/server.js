19a98090fde8714b55e37250fc8db8dc2ec4ea63
nihardarji <ndarji@hawk.iit.edu>
2021-02-28 01:45:30 -0600

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,12 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
